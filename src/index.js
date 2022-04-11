2348fc959e40db2dcd72be1fdedef85d333cffc8
wa26532977 <peterwang_123@yahoo.com>
2022-04-11 18:24:11 -0400

    04/11/2022

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const app = express()
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const server = http.createServer(app)
+const io = socketio(server)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const lobby = {
+    1: {gameId: null, leftPlayer: null, rightPlayer: null},
+    2: {gameId: null, leftPlayer: null, rightPlayer: null},
+    3: {gameId: null, leftPlayer: null, rightPlayer: null},
+    4: {gameId: null, leftPlayer: null, rightPlayer: null},
+    5: {gameId: null, leftPlayer: null, rightPlayer: null},
+}
+
+const livePlayer = {}
+
+function remove_sit(clientId) {
+    Object.keys(lobby).forEach((key) => {
+        if (lobby[key].leftPlayer === livePlayer[clientId]) {
+            lobby[key].leftPlayer = null
+        }
+        if (lobby[key].rightPlayer === livePlayer[clientId]) {
+            lobby[key].rightPlayer = null
+        }
+    })
+}
+
+
+io.on("connection", client => {
+    console.log('a user connected');
+    client.on("user_join_lobby", userName => {
+        livePlayer[client.id] = userName
+        io.emit('lobby_update', lobby, livePlayer)
+    })
+
+    client.on("player_sit", (gameTableId, whichSide) => {
+        // reset previous setting position
+        remove_sit(client.id)
+        // set current position also make sure no one is currently setting here
+        if (lobby[gameTableId][whichSide] === null) {
+            lobby[gameTableId][whichSide] = livePlayer[client.id]
+        }
+        console.log(lobby[gameTableId][whichSide])
+        // update the lobby
+        io.emit('lobby_update', lobby, livePlayer)
+    })
+
+    client.on('player_cancel', () => {
+        remove_sit(client.id)
+        io.emit('lobby_update', lobby, livePlayer)
+    })
+
+    client.on('disconnect', () => {
+        remove_sit(client.id)
+        delete livePlayer[client.id]
+        io.emit('lobby_update', lobby, livePlayer)
+    });
+})
+
+server.listen(process.env.PORT || 8000)
+
